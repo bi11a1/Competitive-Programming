ff5f569ee9ad8fe70953ff3bb6eabca16817294a
Md. Billal Hossain <33820785+bi11a1@users.noreply.github.com>
2021-05-12 18:26:30 +0600

    Converted into class

--- MyAlgo/Sqrt Decomposition-Mo.cpp
+++ MyAlgo/Sqrt Decomposition-Mo.cpp
@@ -1,94 +1,103 @@
-// Problem Link: https://codeforces.com/problemset/problem/86/D
+/// Sample Submission:https://codeforces.com/contest/86/submission/116024412
 
 #include<bits/stdc++.h>
-#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
 using namespace std;
 using ll = long long;
 
-const int kN = 2e5+5;
-const int kBlockSize = sqrt(kN)+1;
-
-int ara[kN], freq[kN*5];
-
-// index are assumed to be within range of [0, kN-1]
+// ----------------begin SqrtDecomposition---------------------
+// Complexity: O((N+Q)sqrt(N))
+// Check value of N
+// Query range [0, n-1]
 struct Query {
   int left, right, index;
   Query() {}
   Query(int new_left, int new_right, int new_index) {
     left = new_left, right = new_right, index = new_index;
   }
-  bool operator<(const Query& new_query) const {
-    int block_no = left/kBlockSize, new_block_no = new_query.left/kBlockSize;
-    if (block_no != new_block_no) { // Smaller blocks first
-      return block_no < new_block_no;
-    } else {  // For even blocks sort in ascending, for odd block sort in descending
-      return (block_no&1) ? (right<new_query.right) : (right>new_query.right);
-    }
-  }
 };
 
-// TODO: Add value at pos and update cur_ans
-void AddQuery(int pos, ll &cur_ans) {
-  cur_ans -= 1LL*freq[ara[pos]]*freq[ara[pos]]*ara[pos];
-  freq[ara[pos]]++;
-  cur_ans += 1LL*freq[ara[pos]]*freq[ara[pos]]*ara[pos];
-}
-
-// TODO: Remove value at pos and update cur_ans
-void RemoveQuery(int pos, ll &cur_ans) {
-  cur_ans -= 1LL*freq[ara[pos]]*freq[ara[pos]]*ara[pos];
-  freq[ara[pos]]--;
-  cur_ans += 1LL*freq[ara[pos]]*freq[ara[pos]]*ara[pos];
-}
-
-// Assuming each query has index [0, n-1]
-vector<ll> SqrtDecomposition(vector<Query> queries) {
-  vector<ll> answers(queries.size());
-  sort(queries.begin(), queries.end());
-
-  // Data structure will hold the cur_ans in range [cur_left, cur_right]
-  int cur_left = 0, cur_right = -1;
-  ll cur_ans = 0;
-  for (Query q : queries) {
-    while (cur_left > q.left) {
-      cur_left--;
-      AddQuery(cur_left, cur_ans);
-    }
-    while (cur_left < q.left) {
-      RemoveQuery(cur_left, cur_ans);
-      cur_left++;
-    }
-    while (cur_right > q.right) {
-      RemoveQuery(cur_right, cur_ans);
-      cur_right--;
-    }
-    while (cur_right < q.right) {
-      cur_right++;
-      AddQuery(cur_right, cur_ans);
-    }
-    answers[q.index] = cur_ans;
+class SqrtDecomposition {
+ public:
+  void AddQuery(Query q) {
+    Queries.push_back(q);
   }
-  return answers;
-}
+
+  // Must pass static/global compare function in sort()
+  static bool CompareQueries(const Query q1, const Query q2) {
+    int block_no1 = q1.left/BlockSize;
+    int block_no2 = q2.left/BlockSize;
+    if (block_no1 != block_no2) { // For two different blocks, keep Smaller one first
+      return block_no1 < block_no2;
+    } else {  // For even blocks sort in ascending, for odd block sort in descending
+      return (block_no1&1) ? (q1.right<q2.right) : (q1.right>q2.right);
+    }
+  }
+
+  ///TODO: Add pos
+  void AddPos(int pos) {
+
+  }
+
+  ///TODO: Remove pos
+  void RemovePos(int pos) {
+
+  }
+
+  ///TODO: Calculate answer
+  int GetAnswer() {
+
+  }
+
+  void ProcessQueries() {
+    sort(Queries.begin(), Queries.end(), CompareQueries);
+    Answers.resize(Queries.size());
+    int cur_left = 0, cur_right = -1;
+    for (Query q : Queries) {
+      while (cur_left > q.left) {
+        cur_left--;
+        AddPos(cur_left);
+      }
+      while (cur_left < q.left) {
+        RemovePos(cur_left);
+        cur_left++;
+      }
+      while (cur_right > q.right) {
+        RemovePos(cur_right);
+        cur_right--;
+      }
+      while (cur_right < q.right) {
+        cur_right++;
+        AddPos(cur_right);
+      }
+      Answers[q.index] = GetAnswer();
+    }
+  }
+
+  /// TODO: Shows answers
+  void ShowAnswers() {
+    for (int i = 0; i < Answers.size(); ++i) {
+      cout << Answers[i] << endl;
+    }
+  }
+
+ private:
+  // static members are initialized once and kept for entire execution of the program
+  static const int N = 2e5+5;
+  static const int BlockSize = sqrt(N)+1;
+  vector<Query> Queries;
+  vector<int> Answers;
+};
+//---------------------end SqrtDecomposition--------------------------------
 
 int main() {
-  FASTIO;
-  int i, j, k;
-  int n, t;
-  cin >> n >> t;
-  vector<Query> queries;
-  for (i = 0; i < n; ++i) cin >> ara[i];
-  for (i = 0; i < t; ++i) {
-    int left, right, index;
-    cin >> left >> right;
-    left--;
-    right--;
-    index = i;
-    queries.push_back(Query(left, right, index));
-  }
+  // Declare object
+  SqrtDecomposition sqd;
 
-  vector<ll> answers = SqrtDecomposition(queries);
-  for (i = 0; i < answers.size(); ++i) {
-    cout << answers[i] << endl;
-  }
+  /// TODO: AddQueries()
+
+  /// TODO: ProcessQueries()
+  sqd.ProcessQueries();
+
+  /// TODO: ShowAnswers()
+  sqd.ShowAnswers();
 }

343d8169bcc2bd38df8bd0bc918a9d4291b4e933
Md. Billal Hossain <33820785+bi11a1@users.noreply.github.com>
2021-06-18 02:27:39 +0600

    Create RMQ.cpp

---
+++ MyAlgo/RMQ.cpp
@@ -0,0 +1,24 @@
+/// Stores the position of the maximum element in array B
+void SetSparseTable() {
+  int i, j, k;
+  for (i = 1; i < MX; ++i) pre_log[i] = log2(i);
+  for (i = 0; i < n; ++i) {
+    spt[i][0] = i;
+  }
+  for (i = 1; i < LG; ++i) {
+    for (j = 0; j+(1<<i)-1 < n; ++j) {
+      if(B[spt[j][i-1]] >= B[spt[j + (1<<(i-1))][i-1]]) {
+        spt[j][i] = spt[j][i - 1];
+      } else {
+				spt[j][i] = spt[j + (1<<(i-1))][i-1];
+      }
+    }
+  }
+}
+
+/// Range Maximum Query on range [l, r] in array B
+int RMQ(int l, int r) {
+  int len = r-l+1;
+  int k = pre_log[len];
+  return max(B[spt[l][k]], B[spt[l + len - (1 << k)][k]]);
+}

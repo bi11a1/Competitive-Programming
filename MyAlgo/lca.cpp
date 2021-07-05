8746855beb1d9c8161a114a6f161f5a16072f71f
Md. Billal Hossain <33820785+bi11a1@users.noreply.github.com>
2021-07-05 14:06:13 +0600

    Followed c++ style guide

--- MyAlgo/lca.cpp
+++ MyAlgo/lca.cpp
@@ -1,10 +1,11 @@
+
 int n;
 int depth[MX], par[MX][lg];
 vector< int > adj[MX];
 
 const int lg=9;
 
-void dfs(int u, int p){
+void Dfs(int u, int p){
   for(auto v:adj[u]){
     if(v==p) continue;
     depth[v]=1+depth[u];
@@ -13,7 +14,7 @@ void dfs(int u, int p){
   }
 }
 
-void set_par(){
+void SetPar(){
   int i, j, k;
   for(i=1; i<lg; ++i){
     for(j=1; j<=n; ++j){
@@ -22,7 +23,7 @@ void set_par(){
   }
 }
 
-int lca(int u, int v){
+int Lca(int u, int v){
   int i, j, k;
   if(depth[u]>depth[v]) swap(u, v);
   int dif=depth[v]-depth[u];

4fbf8e9221a679626aeee12775deff9b930e34b6
Md. Billal Hossain <33820785+bi11a1@users.noreply.github.com>
2021-07-31 16:30:39 +0600

    Initial version without submission

---
+++ MyAlgo/Segment-tree.cpp
@@ -0,0 +1,37 @@
+int tree[4*MX];
+
+void Init(int node, int b, int e) {
+  if (b == e) {
+    /// TODO: tree[node] = ?
+    return;
+  }
+  int lft = (node<<1), rht = lft+1, mid = ((b+e)>>1);
+  Init(lft, b, mid);
+  Init(rht, mid+1, e);
+  ///TODO: tree[node] = ?
+}
+
+void Update(int node, int b, int e, int s, int f) {
+  if (b > f || e < s) return;
+  if (b >= s && e <= f) {
+    /// Todo: tree[node] = ?
+    return;
+  }
+  int lft = (node<<1), rht = lft+1, mid = ((b+e)>>1);
+  Update(lft, b, mid, s, f);
+  Update(rht, mid+1, e, s, f);
+  /// TODO: tree[node] = ?
+}
+
+int Query(int node, int b, int e, int s, int f) {
+  if (b > f || e < s) {
+    return /// TODO: return value for outside segment
+  }
+  if (b >= s && e <= f) {
+    return /// TODO: tree[node] ??
+  }
+  int lft = (node<<1), rht = lft+1, mid = ((b+e)>>1);
+  int q1 = Query(lft, b, mid, s, f);
+  int q2 = Query(rht, mid+1, e, s, f);
+  return /// TODO: q1 or q2 ??
+}

8abca0e6bdbb304125a079d36643ddf1853d6824
Md. Billal Hossain <33820785+bi11a1@users.noreply.github.com>
2021-09-17 12:41:13 +0600

    Update sieve_with_prime_factorization.cpp

--- MyAlgo/sieve_with_prime_factorization.cpp
+++ MyAlgo/sieve_with_prime_factorization.cpp
@@ -4,28 +4,28 @@
 int smallest_pf[MX];
 vector<int> primes;
 void sieve(){
-     int i, j;
-     for(i=2; i<MX; ++i){
-          if(smallest_pf[i]) continue;
-          for(j=i; j<MX; j+=i) smallest_pf[j]=i;
-     }
-     for(i=2; i<MX; ++i){
-          if(smallest_pf[i]==i) primes.push_back(i);
-     }
+  int i, j;
+  for(i=2; i<MX; ++i){
+    if(smallest_pf[i]) continue;
+    for(j=i; j<MX; j+=i) smallest_pf[j]=i;
+  }
+  for(i=2; i<MX; ++i){
+    if(smallest_pf[i]==i) primes.push_back(i);
+  }
 }
 
 ///Returns prime_factors of number X as pairs of <prime, power>
 ///Requires to call sieve()
 ///O(NlogN)
 vector<pair<int,int>> factorize(int X){
-     vector<pair<int,int>> factors;
-     while(X>1){
-          int base=smallest_pf[X], power=0;
-          while(X%base==0){
-               X/=base;
-               power++;
-          }
-          factors.push_back({base, power});
-     }
-     return factors;
+  vector<pair<int,int>> factors;
+  while(X>1){
+    int base=smallest_pf[X], power=0;
+    while(X%base==0){
+      X/=base;
+      power++;
+    }
+    factors.push_back({base, power});
+  }
+  return factors;
 }
